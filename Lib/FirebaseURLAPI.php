4c3cdbf6a467c834210962f9d93e761253e117e8
Ian van den Heuvel <blueangelcpt@gmail.com>
2019-05-13 12:23:46 +0200

    Fixed a minor issue with FDL.

--- Lib/FirebaseURLAPI.php
+++ Lib/FirebaseURLAPI.php
@@ -4,7 +4,10 @@ class FirebaseURLAPI {
 	public function generateShortURL($url) {
 		$key = Configure::read('FirebaseURLAPIServerKey');
 		$curlObj = curl_init();
-		$jsonData = json_encode(array('longDynamicLink' => $url, 'suffix' => array('option' => 'SHORT')));
+		$jsonData = json_encode(array(
+			'longDynamicLink' => 'https://paytoday.page.link/?link=' . $url,
+			'suffix' => array('option' => 'SHORT')
+		));
 		curl_setopt($curlObj, CURLOPT_URL, 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key='.$key);
 		curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
